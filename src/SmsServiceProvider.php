bbb79423d1f48f2ca5b995d4e718f4fd8bf61766
AmirMasoud Sheydaei <amirmasoud.sheydaei@gmail.com>
2018-10-01 10:54:30 +0330

    Initial commit

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Amirmasoud\Magfa;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
